14c3e59a34918fae6b8f5b824001f367e307c409
Hannah257 <Hannah257@users.noreply.github.com>
2020-01-08 22:06:02 +0100

    git commit -m "verlinkung"

---
+++ testordnerTypeScript/testAufgabe_9.ts
@@ -0,0 +1,53 @@
+/*//Globale Variablen
+var todoArray: string [] = ["Aufgabe 1", "Aufgabe 2", "Aufgabe 3"];
+
+//For Schleife 
+for (var index: number = 0; index < todoArray.length; index++) {
+    const element = todoArray[index];
+    console.log(todoArray [1]);  
+}*/
+
+//Globale Variablen
+var todos: string[] = ["kochen", "putzen", "backen"];  //<- mein Array
+
+//Ausgabe in Console
+window.addEventListener("load", function (): void {
+
+    for (var index: number = 0; index < todos.length; index++) {
+        console.log( todos[index] );
+
+    }
+});
+
+//Ausgabe im DOM, V1
+window.addEventListener("load", function (): void {
+
+    for (var index: number = 0; index < todos.length; index++) {
+        document.querySelector("#todos").innerHTML = todos[index];
+    }
+});
+
+//Ausgabe im DOM, V2
+window.addEventListener("load", function (): void {
+
+    for (var index: number = 0; index < todos.length; index++) {
+        document.querySelector("#todos").innerHTML += todos[index];
+    }
+});
+
+//Ausgabe im DOM, V3
+window.addEventListener("load", function (): void {
+
+    for (var index: number = 0; index < todos.length; index++) {
+        document.querySelector("#todos").innerHTML += "<p>ToDo</p>";
+    }
+});
+
+//Ausgabe im DOM, V4
+window.addEventListener("load", function (): void {
+
+    for (var index: number = 0; index < todos.length; index++) {
+        document.querySelector("#todos").innerHTML += "<p>" + todos[index] + "</p>";
+    }
+});
+
